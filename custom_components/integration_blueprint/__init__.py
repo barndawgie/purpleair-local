b9eea8e64c3670e2771da6eac796d8e0c5432531
Josh Barnard <barndawgie@users.noreply.github.com>
2025-05-17 10:13:24 -0700

    Initial setup

--- custom_components/integration_blueprint/__init__.py
+++ custom_components/integration_blueprint/__init__.py
@@ -1,8 +1,8 @@
 """
-Custom integration to integrate integration_blueprint with Home Assistant.
+Custom integration to integrate purpleair_local with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/barndawgie/purpleair_local
 """
 
 from __future__ import annotations
--- custom_components/integration_blueprint/data.py
+++ custom_components/integration_blueprint/data.py
@@ -1,4 +1,4 @@
-"""Custom types for integration_blueprint."""
+"""Custom types for purpleair_local."""
 
 from __future__ import annotations
 
